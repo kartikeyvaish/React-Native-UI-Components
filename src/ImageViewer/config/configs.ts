443991624afb6c494177aa6c0196fe35f71a997c
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-10-13 09:57:25 +0530

    configurations for ImageViewer

---
+++ src/ImageViewer/config/configs.ts
@@ -0,0 +1,139 @@
+const ImageList = [
+    {
+        id: "0",
+        author: "Alejandro Escamilla",
+        width: 5616,
+        height: 3744,
+        uri: "https://picsum.photos/id/0/5616/3744"
+    },
+    {
+        id: "1",
+        author: "Alejandro Escamilla",
+        width: 5616,
+        height: 3744,
+        uri: "https://picsum.photos/id/1/5616/3744"
+    },
+    {
+        id: "10",
+        author: "Paul Jarvis",
+        width: 2500,
+        height: 1667,
+        uri: "https://picsum.photos/id/10/2500/1667"
+    },
+    {
+        id: "100",
+        author: "Tina Rataj",
+        width: 2500,
+        height: 1656,
+        uri: "https://picsum.photos/id/100/2500/1656"
+    },
+    {
+        id: "1000",
+        author: "Lukas Budimaier",
+        width: 5626,
+        height: 3635,
+        uri: "https://picsum.photos/id/1000/5626/3635"
+    },
+    {
+        id: "1001",
+        author: "Danielle MacInnes",
+        width: 5616,
+        height: 3744,
+        uri: "https://picsum.photos/id/1001/5616/3744"
+    },
+    {
+        id: "1002",
+        author: "NASA",
+        width: 4312,
+        height: 2868,
+        uri: "https://picsum.photos/id/1002/4312/2868"
+    },
+    {
+        id: "1003",
+        author: "E+N Photographies",
+        width: 1181,
+        height: 1772,
+        uri: "https://picsum.photos/id/1003/1181/1772"
+    },
+    {
+        id: "1004",
+        author: "Greg Rakozy",
+        width: 5616,
+        height: 3744,
+        uri: "https://picsum.photos/id/1004/5616/3744"
+    },
+    {
+        id: "1005",
+        author: "Matthew Wiebe",
+        width: 5760,
+        height: 3840,
+        uri: "https://picsum.photos/id/1005/5760/3840"
+    },
+    {
+        id: "1006",
+        author: "Vladimir Kudinov",
+        width: 3000,
+        height: 2000,
+        uri: "https://picsum.photos/id/1006/3000/2000"
+    },
+    {
+        id: "1008",
+        author: "Benjamin Combs",
+        width: 5616,
+        height: 3744,
+        uri: "https://picsum.photos/id/1008/5616/3744"
+    },
+    {
+        id: "1009",
+        author: "Christopher Campbell",
+        width: 5000,
+        height: 7502,
+        uri: "https://picsum.photos/id/1009/5000/7502"
+    },
+    {
+        id: "101",
+        author: "Christian Bardenhorst",
+        width: 2621,
+        height: 1747,
+        uri: "https://picsum.photos/id/101/2621/1747"
+    },
+    {
+        id: "1010",
+        author: "Samantha Sophia",
+        width: 5184,
+        height: 3456,
+        uri: "https://picsum.photos/id/1010/5184/3456"
+    },
+    {
+        id: "1011",
+        author: "Roberto Nickson",
+        width: 5472,
+        height: 3648,
+        uri: "https://picsum.photos/id/1011/5472/3648"
+    },
+    {
+        id: "1012",
+        author: "Scott Webb",
+        width: 3973,
+        height: 2639,
+        uri: "https://picsum.photos/id/1012/3973/2639"
+    },
+    {
+        id: "1013",
+        author: "Cayton Heath",
+        width: 4256,
+        height: 2832,
+        uri: "https://picsum.photos/id/1013/4256/2832"
+    },
+    {
+        id: "1015",
+        author: "Alexey Topolyanskiy",
+        width: 6000,
+        height: 4000,
+        uri: "https://picsum.photos/id/1015/6000/4000"
+    },
+]
+
+export default {
+    ImageList
+}
